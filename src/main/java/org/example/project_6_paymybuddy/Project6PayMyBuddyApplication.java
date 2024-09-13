c072d974c4608eb5441292dd192ac204f223ec1d
Joel LAO <joel.lao@axa.fr>
2024-09-13 10:42:45 +0200

    Initializing Project

---
+++ src/main/java/org/example/project_6_paymybuddy/Project6PayMyBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.example.project_6_paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project6PayMyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project6PayMyBuddyApplication.class, args);
+	}
+
+}
