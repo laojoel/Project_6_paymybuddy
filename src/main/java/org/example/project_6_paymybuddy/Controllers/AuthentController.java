b6325c346a047769fc6d4029c5f8b54877870dde
Joel LAO <joel.lao@axa.fr>
2024-09-18 17:55:20 +0200

    Add Authent System with SHA-256 Hash for password

--- src/main/java/org/example/project_6_paymybuddy/Controllers/AuthentController.java
+++ src/main/java/org/example/project_6_paymybuddy/Controllers/AuthentController.java
@@ -1,8 +1,12 @@
 package org.example.project_6_paymybuddy.Controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.example.project_6_paymybuddy.Services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -10,9 +14,19 @@ public class AuthentController {
 
     //private final UserService userService;
 
-    @GetMapping("/login")
-    public String login() {
-        UserService.getUserName();
-        return "login";
+    @GetMapping("/signin")
+    public String getSignIn() {
+        System.out.println("Get Sign In");
+        return "signin";
+    }
+    @GetMapping("/signup")
+    public String getSignUp() {
+        return "signup";
+    }
+
+    @PostMapping("/signin")
+    public String PostSignIn(@RequestParam("mail") String mail, @RequestParam("password") String password) {
+        System.out.println("mail: " + mail + " | password: " + password);
+        return "signin";
     }
 }
